5c4a6baf8c2881da70e1472177e9ee57de91b1f6
Shaun Fitzgibbon <shaun@fitzindustries.com>
2023-02-06 19:42:37 +0200

    amended tmux and zsh

--- dot_config/nvim/lua/personal/plugins-setup.lua
+++ dot_config/nvim/lua/personal/plugins-setup.lua
@@ -1,15 +1,18 @@
 -- auto install packer if not installed
 local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+	local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	local is_bootstrap = false
+	if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+		is_bootstrap = true
+		vim.fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
 		vim.cmd([[packadd packer.nvim]])
 		return true
 	end
 	return false
 end
 local packer_bootstrap = ensure_packer() -- true if packer was just installed
+
+
 
 -- import packer safely
 local status, packer = pcall(require, "packer")
